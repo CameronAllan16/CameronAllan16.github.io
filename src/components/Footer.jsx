358cde94e7bca9f85450f157902ee23bc185d495
cameronallan <camallan16@gmail.com>
2023-03-28 15:48:16 -0400

    Add projects page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import  "../assets/footer.css"
+
+function Footer() {
+    return (
+      <div className="footer">
+        <div className="footer-content-container" >
+          <div className="footer-content">
+            Link
+          </div>
+          <div className="footer-content">
+                Links
+          </div>
+          <div className="footer-content">
+                Linksy
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  export default Footer;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,15 @@
+import  "../assets/projects.css"
+
+function Projects() {
+    return (
+      <div className="Projects">
+        <header className="Projects-header-container" >
+          <p className="projects-header">
+            These are my projects
+          </p>
+        </header>
+      </div>
+    );
+  }
+
+  export default Projects;
