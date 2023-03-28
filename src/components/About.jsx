455b0a289d0a349761d6e367082fdd97edd53dd3
cameronallan <camallan16@gmail.com>
2023-03-28 15:47:14 -0400

    Add About section

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import  "../assets/about.css"
+
+function About() {
+    return (
+      <div className="about">
+        <header className="about-header-container" >
+          <p className="about-header">
+            About me
+          </p>
+        </header>
+      </div>
+    );
+  }
+
+  export default About;
