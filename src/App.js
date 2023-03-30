791720c47c2f0d14c55dab5532009bca6b616536
cameronallan <camallan16@gmail.com>
2023-03-30 14:43:04 -0400

    Remove unused router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './assets/App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Portfolio from './components/Portfolio';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Portfolio/>} />
-      </Routes>
-    </BrowserRouter>
+    <Portfolio/>
   );
 }
 
