c7458a64bf6d3bebdff8be163abc0cd85e907d9b
Cameron Allan <camallan16@gmail.com>
2023-04-06 15:05:22 -0400

    About section changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,23 @@
-import  "../assets/about.css"
+import "../assets/about.css";
+import ProfileImage from "../assets/pic-of-me.JPG";
 
 function About() {
-    return (
-      <div className="about">
-        <header className="about-header-container" >
-          <h1 className="about-header">
-            About
-          </h1>
-          <div className="about-body">
-          <p className="about-description">
-            This should be a description.
-          </p>
-          </div>
-
-        </header>
+  return (
+    <div className="about">
+      <h2 className="section-title">About Me</h2>
+      <div className="row">
+        <div className="col-md-6">
+          <img className="profile-image" src={ProfileImage} alt="Image of me" />
+        </div>
+        <div className="col-md-6">
+          <h3>Hi, I'm Cameron Allan</h3>
+          <p>I'm a frontend developer from Noblesville, Indiana.</p>
+          <p>I specialize in creating clean and modern user interfaces using ReactJS and VueJS.</p>
+          <p>When I'm not coding, you can find me spending time with my family, playing video games, and exploring the world of photography.</p>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default About;
+export default About;
