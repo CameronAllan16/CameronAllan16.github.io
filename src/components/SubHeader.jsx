2eabda4b34e0bac23a191939d5ef1ed65639ac74
Cameron Allan <camallan16@gmail.com>
2023-04-06 15:05:54 -0400

    update header

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -1,18 +1,11 @@
-import  "../assets/subheader.css"
+import "../assets/subheader.css";
 
 function Subheader(props) {
-    return (
-      <div className="subheader">
-        <div className="subheader-image">
-          <img src={props.backgroundImage} alt="Background" />
-          <h1 className="subheader-name">{props.name}</h1>
-        </div>
-        <div className="subheader-text">
-          <h2>{props.title}</h2>
-          <p>{props.description}</p>
-        </div>
-      </div>
-    );
-  }
-  
-  export default Subheader;
+  return (
+    <header className="subheader">
+      <h1 className="name">{props.name}</h1>
+    </header>
+  );
+}
+
+export default Subheader;
