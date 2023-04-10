7cafdfddef92b4a05d0882492b1e86abfe1f8fd7
Cameron Allan <camallan16@gmail.com>
2023-04-10 15:26:16 -0400

    Remove redundant lines

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,12 +6,12 @@ function Projects() {
       <h2 className="section-title">My Work</h2>
       <div className="row">
         <div className="col-sm-6">
-          <img className="project-image" src="image-1.jpg" alt="Image 1" />
+          <img className="project-image" src="image-1.jpg" />
           <h3>Estate Vault</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className="col-sm-6">
-          <img className="project-image" src="image-2.jpg" alt="Image 2" />
+          <img className="project-image" src="image-2.jpg" />
           <h3>Cooking Assistant</h3>
           <p>
             Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
