bde8653ca31bd6dd8c7d56449006f6dbbee4f1e6
Cameron Allan <camallan16@gmail.com>
2023-04-10 16:12:32 -0400

    Remove Router to SPA

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './assets/App.css';
 import Portfolio from './components/Portfolio';
 
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Portfolio />} />
-      </Routes>
-    </BrowserRouter>
+   
+          <Portfolio />
+
   );
 }
 
