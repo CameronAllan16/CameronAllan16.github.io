8ac34725d368c695abe8a093914ea1146a813ac0
Cameron Allan <camallan16@gmail.com>
2024-06-14 09:57:22 -0400

    begin content generation for home page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,24 @@
-import "../assets/portfolio.css";
-import Footer from "./Footer";
-import NavBar from "./NavBar";
+import '../assets/portfolio.css';
+import Footer from './Footer';
+import NavBar from './NavBar';
 
 function Portfolio() {
   return (
     <div className="app">
-      <NavBar/>
-      <p className="content">Some content will soon be here.</p>
+      <NavBar />
+
+      <div className="content-wrapper">
+        <div className="content">
+          <div className='header-wrapper'>
+            <div className='header'>
+                <h2>Hello, my name is Cameron</h2>
+            </div>
+          </div>
+          <div className="body">
+            <p className="body-content">Some content will soon be here.</p>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
